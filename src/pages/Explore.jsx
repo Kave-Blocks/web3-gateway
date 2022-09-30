2129deb0eb99ad2778900fccace282d2520ae8b1
chedj09 <cherryl@kaveblocks.com>
2022-09-30 21:24:19 +0800

    additional button for load more and additional section for participants

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -10,12 +10,15 @@ function Explore() {
   return (
     <>
       <section className="relative container-fluid p-10 m-10">
+        {/* Circle Gradient */}
+        <div className="absolute z-[0] w-[15%] h-[15%] right-0 top-10 orange__gradient"></div>
+
         <div className="text-white md:text-5xl text-4xl font-bold p-16 text-center">
           {/* <img className="absolute" src={circle} alt="" /> */}
           <h1>Explore our Web3 Courses</h1>
         </div>
 
-        <div className="container grid md:grid-cols-2 gap-10 text-white content-evenly">
+        <div className="container grid md:grid-cols-2 gap-10 text-white content-evenly p-10">
           {/* Bitcoin */}
           <div className="grid grid-cols-2 gap-4 p-10 border border-gray-700 card-explore ">
             <div>
@@ -50,6 +53,9 @@ function Explore() {
             </div>
           </div>
 
+          {/* Circle Gradient */}
+          <div className="absolute z-[0] w-[20%] h-[15%] bottom-10 orange__gradient"></div>
+
           {/* Connect Wallet */}
           <div className="grid grid-cols-2 gap-4 p-10 border border-gray-700 card-explore ">
             <div>
@@ -83,6 +89,19 @@ function Explore() {
               <img className=" m-auto" src={nft} alt="" />
             </div>
           </div>
+        </div>
+
+        {/* Load More Button */}
+        <div className="text-center p-10">
+          <button className="border border-orange-400 rounded-xl px-14 py-2 text-white text-xl card-explore">
+            <a href="">Load More</a>
+          </button>
+        </div>
+
+        {/* Participants Section */}
+        <div className="p-10">
+          <h1 className="text-white text-4xl font-bold  p-3">Participants</h1>
+          <div className="border-b-2 border-slate-600 p-2"></div>
         </div>
       </section>
     </>
